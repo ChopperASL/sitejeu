88009d46aa220fd6d6cc43f7daccbd2a98ed66b4
bierl <loic.bier@ynov.com>
2026-05-20 10:31:29 +0200

    ajout serveur

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/font', express.static(path.join(__dirname, 'font')));
+app.use('/documents', express.static(path.join(__dirname, 'documents')));
+
+// routes vers chaque page HTML
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'html', 'index.html')));
+app.get('/apprendre', (req, res) => res.sendFile(path.join(__dirname, 'html', 'apprendre.html')));
+app.get('/jouer', (req, res) => res.sendFile(path.join(__dirname, 'html', 'jouer.html')));
+app.get('/mentions-legales', (req, res) => res.sendFile(path.join(__dirname, 'html', 'mentions-legales.html')));
+
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
